8749a39d662bc6a2a889f994455c920a78cf353b
Sakuraifikar <fikaryourwelcome@gmail.com>
2022-02-17 13:24:26 +0700

    commit 1

--- app/src/main/java/com/example/profilsekolah2/EkskulActivity.java
+++ app/src/main/java/com/example/profilsekolah2/EkskulActivity.java
@@ -1,6 +1,8 @@
 package com.example.profilsekolah2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -29,5 +31,25 @@ public class EkskulActivity extends AppCompatActivity {
         sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
         sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
         sliderView.startAutoCycle();
+    }
+
+    public void osis(View view) {
+        Intent intent = new Intent(EkskulActivity.this, OsisActivity.class);
+        startActivity(intent);
+    }
+
+    public void pramuka(View view) {
+        Intent intent = new Intent(EkskulActivity.this, PramukaActivity.class);
+        startActivity(intent);
+    }
+
+    public void pmr(View view) {
+        Intent intent = new Intent(EkskulActivity.this, PmrActivity.class);
+        startActivity(intent);
+    }
+
+    public void futsal(View view) {
+        Intent intent = new Intent(EkskulActivity.this, FutsalActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/profilsekolah2/GaleriActivity.java
+++ app/src/main/java/com/example/profilsekolah2/GaleriActivity.java
@@ -3,12 +3,30 @@ package com.example.profilsekolah2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ViewFlipper;
 
 public class GaleriActivity extends AppCompatActivity {
+    ViewFlipper viewFlipper;
+    Animation fadein, fadeout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_galeri);
+
+        viewFlipper = (ViewFlipper) findViewById(R.id.viewflipper);
+
+        fadein = AnimationUtils.loadAnimation(this,R.anim.fade_in);
+        fadeout = AnimationUtils.loadAnimation(this,R.anim.fade_out);
+
+        viewFlipper.setInAnimation(fadein);
+        viewFlipper.setOutAnimation(fadein);
+
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.startFlipping();
+
     }
 }
