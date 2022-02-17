c9d72c8dbee3af0906802c99462110462a629454
Sakuraifikar <fikaryourwelcome@gmail.com>
2022-02-17 09:53:21 +0700

    commit 1

--- app/src/main/java/com/example/profilsekolah2/EkskulActivity.java
+++ app/src/main/java/com/example/profilsekolah2/EkskulActivity.java
@@ -1,14 +1,33 @@
 package com.example.profilsekolah2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
 public class EkskulActivity extends AppCompatActivity {
+    SliderView sliderView;
+    int[] images = {R.drawable.eskul1,
+            R.drawable.eskul2,
+            R.drawable.eskul1,
+            R.drawable.eskul2,
+            R.drawable.eskul1};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ekskul);
+
+        sliderView = findViewById(R.id.image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
     }
 }
